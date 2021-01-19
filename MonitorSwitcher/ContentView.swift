c2f56e3ec18b68d7951082a592e9b11f456da757
Kyle Partridge <kpartridge@riotgames.com>
2021-01-19 14:04:07 -0800

    Initial Commit

---
+++ MonitorSwitcher/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MonitorSwitcher
+//
+//  Created by Kyle Partridge on 1/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
